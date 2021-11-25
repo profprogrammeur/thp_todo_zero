652f033746b703f1c9496f193d515202b3b5d1dd
profprogrammeur <contact@profprogrammeur.com>
2021-11-26 00:58:18 +0100

    read front

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -28,25 +28,32 @@ class EmailsController < ApplicationController
 
 
    def show
-     
     @email = Email.find(params[:id])
     respond_to do |format|
-          format.html { redirect_to emails_path }
-          format.js { }
-          flash[:notice] = "Email created"
-        end
-
+        format.html { redirect_to emails_path }
+        format.js { }
+        flash[:notice] = "Email created"
+      end
   end
 
-    def destroy
-      @email = Email.find(params[:id])
-      @email.destroy
-      respond_to do |format|
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    respond_to do |format|
       format.html { redirect_to emails_path }
       format.js { }
       flash[:notice] = "Email destroyed"
-    end
+      end
+  end
 
+    def update
+      @email = Email.find(params[:id])
+      # @email.read
+      respond_to do |format|
+        format.html { redirect_to emails_path }
+        format.js { }
+        flash[:notice] = "Email read"
+      end
   end
 
 end
